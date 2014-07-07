5a5f7e862a3393f7e15a92783ded51b33780eebd
mohankokkula <mohankokkula@gmail.com>
2014-07-07 16:55:42 +0530

    this is myapp jp...

---
+++ exgit/src/info/hub/MyApp.java
@@ -0,0 +1,12 @@
+package info.hub;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi this is from myapp...");
+
+	}
+
+}
